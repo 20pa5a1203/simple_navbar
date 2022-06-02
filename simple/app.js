aeaa7464622dd9241457851a8683734705ae2b90
20pa5a1203 <96017135+20pa5a1203@users.noreply.github.com>
2022-06-02 23:16:12 +0530

    Add files via upload

---
+++ simple/app.js
@@ -0,0 +1,39 @@
+// var num=20;
+// for(let i=1;i<=20;i++){
+//     var flag=0;
+//     for(let j=2;j<i;j++){
+//         if(i%j==0){
+//             flag=1;
+//             break
+//         }
+//     }
+//     if(flag==1){
+//         console.log(i,"not a pime");
+//     }
+//     else{
+//         console.log(i,"is a prime");
+//     }
+// }
+
+
+
+var logo=document.getElementById("logo");
+logo.addEventListener("click",function(){
+    window.open("https://www.netflix.com/in/","_self");
+});
+var home=document.getElementById("home");
+home.addEventListener("click",function(){
+    window.open("https://www.netflix.com/in/");
+});
+var menu=document.getElementById("menu");
+menu.addEventListener("click",function(){
+    window.open("https://www.netflix.com/in/");
+});
+var contact=document.getElementById("contact");
+contact.addEventListener("click",function(){
+    window.open("https://www.netflix.com/in/");
+});
+var about=document.getElementById("about");
+about.addEventListener("click",function(){
+    window.open("https://www.netflix.com/in/");
+});
